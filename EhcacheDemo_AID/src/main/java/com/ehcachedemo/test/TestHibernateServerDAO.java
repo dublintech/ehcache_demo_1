fc881570fff69dc8c714e474776cdd20a2ab933f
A U Thor <author@example.com>
2012-06-24 12:07:28 +0100

    added junit4 and sqllogging to hibernate

--- EhcacheDemo_AID/src/main/java/com/ehcachedemo/test/TestHibernateServerDAO.java
+++ EhcacheDemo_AID/src/main/java/com/ehcachedemo/test/TestHibernateServerDAO.java
@@ -4,6 +4,8 @@ import com.ehcachedemo.dao.*;
 import com.ehcachedemo.pojo.*;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -15,57 +17,89 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import org.hibernate.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.stat.EntityStatistics;
 import org.hibernate.stat.Statistics;
+import org.junit.Test;
 
+import org.junit.Test;
+
+
+/**
+ * Tests Hibernate Second level Caching.
+ * <P>
+ * Hibernate maintains two levels of cache: first-level and second-level cache. 
+ * The first-level cache is responsible for storing the results within a particular session instance, while the second-level 
+ * cache is associated with the SessionFactory instance.
+ * <P>
+ * The second-level cache can be enabled or disabled by setting the property hibernate.cache.use_second_level_cache to
+ * true (the default for classes that specify <cache> mapping) or false, respectively.
+ * <P>
+ * You can choose which implementation to use for an application by setting the hibernate.cache.provider_class property 
+ * in the hibernate.cfg.xml file as follows.
+ * <Pre>
+ * <property name="hibernate.cache.provider_class">org.hibernate.cache.EhCacheProvider</property>
+ * </pre>
+ * <P>
+ * Caching can also be configured for each entity by annotating the annotation as:
+ * <pre>
+ * @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+ * </pre>
+ * 
+ * <P>
+ * References:
+ * <UL>
+ * <LI>http://www.developer.com/java/ent/article.php/10933_3892261_2/JPA-20-Cache-Vs-Hibernate-Cache-Differences-in-Approach.htm
+ * <LI>http://docs.jboss.org/hibernate/orm/4.0/devguide/en-US/html/ch06.html
+ * </UL>
+ * 
+ * Next up:
+ * 1. Write multi-threaded read
+ * 2. Write tests
+ * 3. See if there is any way to make database slower.
+ * 4. Go thru various cache config options.
+ * 
+ * @author Alex
+ *
+ */
 public class TestHibernateServerDAO {
 	
+	@Test
 	public void testGetServer() {
 		Cache cache = HibernateUtil.getSessionFactory().getCache();
 		HibernateServerDAO hsDAO = new HibernateServerDAO();
-		// execute something you don't care about just to get rid of first time outlier.
-		hsDAO.getServer(187);
 		for (int i = 0; i < 50; i++) {
 			long beginTime = System.currentTimeMillis();
-			Server servers = hsDAO.getServer(186);
+			System.out.println("Cache contains server=" + cache.containsEntity(Server.class, 186));
+			Server server = hsDAO.getServer(186);
 			long endTime = System.currentTimeMillis();
 			System.out.println("Iteration " + i + ", time = " + (endTime - beginTime));
-			// for a laugh check if server with primary key 186 is in cache.
-			//System.out.println("in cache=" + 
-			//		em.getEntityManagerFactory().getCache().contains(Server.class, 186));
-			System.out.println("cache is=" + cache);
 			if (i % 5 == 0) {
+				System.out.println("Eviction happeing");
 				cache.evictEntityRegion(Server.class);
 			}
 		}
 	}
 	
+	@Test
 	public void testGetNonCachedServer() {
 		Cache cache = HibernateUtil.getSessionFactory().getCache();
 		HibernateServerDAO hsDAO = new HibernateServerDAO();
-		// execute something you don't care about just to get rid of first time outlier.
-		hsDAO.getNonCachedServer(187);
 		for (int i = 0; i < 50; i++) {
+			System.out.println("Cache contains server=" + cache.containsEntity(NonCachedServer.class, 186));
 			long beginTime = System.currentTimeMillis();
 			NonCachedServer server = hsDAO.getNonCachedServer(186);
 			long endTime = System.currentTimeMillis();
 			System.out.println("Iteration " + i + ", time = " + (endTime - beginTime));
-			// for a laugh check if server with primary key 186 is in cache.
-			//System.out.println("in cache=" + 
-			//		em.getEntityManagerFactory().getCache().contains(Server.class, 186));
-			System.out.println("cache is=" + cache);
 			if (i % 5 == 0) {
 				cache.evictEntityRegion(Server.class);
 			}
 		}
 	}
 	
+	@Test
 	public void testListAllServers() {
 		Cache cache = HibernateUtil.getSessionFactory().getCache();
-
-		// for a laugh check if server with primary key 186 is in cache.
-		//System.out.println("in cache=" + 
-		//			em.getEntityManagerFactory().getCache().contains(Server.class, 186));
 		HibernateServerDAO hsDAO = new HibernateServerDAO();
 		for (int i = 0; i < 20; i++) {
 			long beginTime = System.currentTimeMillis();
--- EhcacheDemo_AID/src/main/java/com/ehcachedemo/test/TestRunner.java
+++ EhcacheDemo_AID/src/main/java/com/ehcachedemo/test/TestRunner.java
@@ -7,15 +7,26 @@ import com.ehcachedemo.dao.HibernateUtil;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.engine.SessionFactoryImplementor;
+import org.junit.Test;
 
 /**
  * <B>EhCache Threads</B>
  * In this example, EhCache spawns two threads:
  * <OL>
  * <LI>Daemon Thread: net.sf.ehcache.CacheManager
- * 
  * <LI>Non Daemon Thread: com.ehcachedemo.pojo.Server.data
  * </OL>
+ * when it is configured as: 
+ * <pre>
+ * overflowToDisk="true"
+ * </pre>
+ * When, 
+ * <pre>
+ * overflowToDisk="true"
+ * </pre>
+ * , EhCache spawns one thread w
+ * 
+ * 
  *  
  * <B>EhCache Shutdown</B>
  * http://ehcache.org/documentation/operations/shutdown
@@ -24,16 +35,12 @@ import org.hibernate.engine.SessionFactoryImplementor;
  * Info on EhCache - aggregate http://twasink.net/2005/10/20/ehcache-dissected/
  * 
  * @author Alex
+ * 
  *
  */
 public class TestRunner {
 
-	public static void main(String args[]) {
-		TestRunner testRunner = new TestRunner();
-		testRunner.testHibernate_EH_Cache();
-	}
-
-	
+	@Test
 	public void testHibernate_EH_Cache() {
 		// output cache name
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
@@ -42,8 +49,6 @@ public class TestRunner {
 			    .getName();
 		
 		System.out.println(">> cacheIntegrationName=" + cacheIntegrationName);
-
-		
 		
 		TestHibernateServerDAO testHibernateServerDAO = 
 				new TestHibernateServerDAO();
